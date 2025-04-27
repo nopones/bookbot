5bd402ac6aae2507812e95839b656c13b9e6ca6e
nopones <macdi19@gmail.com>
2025-04-27 13:49:24 -0600

    Added read function to main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("greetings boots")
+
+def get_book_text(path):
+    with open (path) as book:
+        file_contents = book.read()
+        print (file_contents)
+
+def main():
+    path="books/"+"frankenstein.txt"
+    get_book_text(path)
+
+main()

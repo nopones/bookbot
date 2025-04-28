062ccfa496f7e98e6dda9f25b50ff41f104c234a
Mario Cuellar Diaz <mariocuellardiaz@MacBook-Air-de-Mario.local>
2025-04-27 23:16:41 -0600

    Created stats.py and refracted main.py

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
+from stats import count_words
 
 def get_book_text(path):
     with open (path) as book:
         file_contents = book.read()
         return file_contents
-
-def count_words(string):
-    list_of_words=string.split()
-    words= len(list_of_words)
-    return (words)
 
 def main():
     path="books/"+"frankenstein.txt"
---
+++ stats.py
@@ -0,0 +1,4 @@
+def count_words(string):
+    list_of_words=string.split()
+    words= len(list_of_words)
+    return (words)

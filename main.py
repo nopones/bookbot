ef7d9eba37f1d57b263e8b1281fa95fcd0ce98bf
Mario Cuellar Diaz <mariocuellardiaz@MacBook-Air-de-Mario.local>
2025-04-28 13:41:29 -0600

    Added feature to add book path

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import count_words,count_characters,sort_on,sorted_list
+import sys
 
 def get_book_text(path):
     with open (path) as book:
@@ -6,18 +7,23 @@ def get_book_text(path):
         return file_contents
 
 def main():
-    path="books/"+"frankenstein.txt"
-    print("============ BOOKBOT ============")
-    print(f"Analyzing book found at {path}...")
-    print("----------- Word Count ----------")
-    book_text=get_book_text(path)
-    words_counted=count_words(book_text)
-    print(f"Found {words_counted} total words")
-    print("--------- Character Count -------")
-    characters_counted=count_characters(book_text)
-    characters_list=sorted_list(characters_counted)
-    for char_dict in characters_list:
-        if char_dict["char"].isalpha():
-            print(f"{char_dict['char']}: {char_dict['num']}")
-    print("============= END ===============")
+    if len(sys.argv)==2:
+        path=(sys.argv[1])
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at {path}...")
+        print("----------- Word Count ----------")
+        book_text=get_book_text(path)
+        words_counted=count_words(book_text)
+        print(f"Found {words_counted} total words")
+        print("--------- Character Count -------")
+        characters_counted=count_characters(book_text)
+        characters_list=sorted_list(characters_counted)
+        for char_dict in characters_list:
+            if char_dict["char"].isalpha():
+                print(f"{char_dict['char']}: {char_dict['num']}")
+        print("============= END ===============")
+    else:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+        
 main()
